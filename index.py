7a6d54955c517a90ec300e024433986ee9f9a8c2
yun920722 <s1101686@gm.pu.edu.tw>
2022-10-26 19:09:26 +0800

    make it better

--- index.py
+++ index.py
@@ -7,9 +7,6 @@ app = Flask(__name__)
 def index():
     homepage = "<h1>黃品芸的求職資訊網頁</h1>"
     homepage += "<a href=/me>自我介紹</a><br>"
-    homepage += "<a href=/today>顯示日期時間</a><br>"
-    homepage += "<a href=/welcome?nick=tcyang>傳送使用者暱稱</a><br>"
-    homepage += "<a href=/about>子青簡介網頁</a><br>"
     return homepage
 
 @app.route("/me")
