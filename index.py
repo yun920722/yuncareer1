e7c876c17251c52ded1658413f98721c12861682
yun920722 <s1101686@gm.pu.edu.tw>
2022-10-26 19:56:26 +0800

    make it better

--- index.py
+++ index.py
@@ -7,11 +7,23 @@ app = Flask(__name__)
 def index():
     homepage = "<h1>黃品芸的求職資訊網頁</h1>"
     homepage += "<a href=/me>自我介紹</a><br>"
+    homepage += "<a href=/mis>MIS工作介紹</a><br>"
+    homepage += "<a href=/career>職涯測驗結果</a><br>"
+    homepage += "<a href=/me1>求職自傳</a><br>"
     return homepage
 
 @app.route("/me")
 def route():
     return render_template("me.html")
+@app.route("/mis")
+def route():
+    return render_template("mis.html")
+@app.route("/career")
+def route():
+    return render_template("career.html")
+@app.route("/me1")
+def route():
+    return render_template("me1.html")
 
 #if __name__ == "__main__":
 #    app.run()
